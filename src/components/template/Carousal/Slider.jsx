3ed5924f05b0b6eef65867d0bdc887cac6c3ad96
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-05-26 16:59:44 +0500

    slider completed

--- src/components/template/Carousal/Slider.jsx
+++ src/components/template/Carousal/Slider.jsx
@@ -3,42 +3,101 @@
 import { useEffect, useRef } from 'react';
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
-import Image from 'next/image';
 import {ASSETS} from '../../../../public/Assets'
+import Image from 'next/image';
+import logo from '../../../../public/icon/days.svg'
 
 const SwiperComponent = () => {
   const slides = [
     {
-      "src": "../../../../public/images/portugal.jpg",
-      "title": "Australia",
+      "src": ASSETS.itely,
+      "title": "itely",
+      "cost":"$15k",
+      "trip":"28 days"
     },
     {
-      "src": ASSETS.finland,
-      "title": "Finland",
+      "src": ASSETS.thailand,
+      "title": "thailand",
+      "cost":"$5k",
+      "trip":"8 days"
     },
     {
-      "src": ASSETS.itley,
-      "title": "Lake como italian",
+      "src": ASSETS.south_africa,
+      "title": "south africa",
+      "cost":"$20k",
+      "trip":"20 days"
     },
     {
       "src": ASSETS.japan,
-      "title": "Japan",
+      "title": "japan",
+      "cost":"$25k",
+      "trip":"18 days"
     },
     {
-      "src": ASSETS.kaung,
-      "title": "Kuang Si Falls",
+      "src": ASSETS.spain,
+      "title": "spain",
+      "cost":"$10k",
+      "trip":"10 days"
     },
     {
-      "src": ASSETS.muchai,
-      "title": "Mu Cang Chai",
+      "src": ASSETS.columbia,
+      "title": "columbia",
+      "cost":"$15k",
+      "trip":"28 days"
     },
     {
-      "src": ASSETS.portugal,
-      "title": "Portugal",
+      "src": ASSETS.france,
+      "title": "france",
+      "cost":"$35k",
+      "trip":"28 days"
     },
     {
-      "src": ASSETS.usa,
-      "title": "Joshua Tree USA",
+      "src": ASSETS.indonesia,
+      "title": "indonesia",
+      "cost":"$35k",
+      "trip":"38 days"
+    },
+    {
+      "src": ASSETS.canada,
+      "title": "canada",
+      "cost":"$1k",
+      "trip":"2 days"
+    },
+    {
+      "src": ASSETS.india,
+      "title": "india",
+      "cost":"$5k",
+      "trip":"8 days"
+    },
+    {
+      "src": ASSETS.australia,
+      "title": "australia",
+      "cost":"$1k",
+      "trip":"2 days"
+    },
+    {
+      "src": ASSETS.USA,
+      "title": "USA",
+      "cost":"$5k",
+      "trip":"18 days"
+    },
+    {
+      "src": ASSETS.brazil,
+      "title": "brazil",
+      "cost":"$115k",
+      "trip":"28 days"
+    },
+    {
+      "src": ASSETS.mexico,
+      "title": "mexico",
+      "cost":"$10k",
+      "trip":"18 days"
+    },
+    {
+      "src": ASSETS.china,
+      "title": "china",
+      "cost":"$35k",
+      "trip":"26 days"
     }
   
 
@@ -53,10 +112,10 @@ const SwiperComponent = () => {
       loop: true,
       loopedSlides: 16,
       speed: 700,
-      // autoplay: {
-      //   delay: 1500,
-      //   disableOnInteraction: false,
-      // },
+      autoplay: {
+        delay: 1500,
+        disableOnInteraction: true,
+      },
       centeredSlides: true,
       breakpoints: {
         400: { slidesPerView: 1 },
@@ -79,34 +138,59 @@ const SwiperComponent = () => {
       <div className="swiper-container" ref={swiperRef}>
         <ul className="swiper-wrapper ag-slide_list">
           {slides.map((slide, index) => (
-            <li className="swiper-slide ag-slide_item" key={index}>
-            {/* <li className="swiper-slide ag-slide_item" key={index} data-swiper-autoplay="1500"> */}
-              <div className="flex flex-col">
+      <li className="swiper-slide ag-slide_item" key={index} data-swiper-autoplay="1500"> 
+              <div className="flex flex-col ">
 
-               <div
-               style={{
-                 height:"220px",
-                margin:"0 44px",
-                borderRadius:"20px",
-                position:"relative",
-              }}
-               >
-<Image
+            
+<img
 src={slide.src}
 alt={slide.title}
-layout='fill'
+height={220}
+style={{margin:"0 34px", borderRadius:"19px" ,height:"220px" }}
 className="object-cover"
 />
-               </div>
               <div
-              className='shadow-xl'
                 style={{
-                  margin:"0 44px",  
-                  height:"100px",
-
+                  margin:"0 34px",  
+                  boxSizing:"border-box",
+                  padding:"10px",
+                  height:"180px",
                 }}
               >
-         <h6 className="text-lg mb-8 font-bold ">{slide.title}</h6>
+                <div style={
+                  {
+                    display:"flex",
+                    justifyContent:"space-between",
+                    width:"100%",
+                    padding:"0 7px"
+                  }
+                  
+                }> 
+
+         <h6 className="font-semibold capitalize text-lg ">{slide.title}</h6>
+         <h6 className="text-lg  font-semibold  ">{slide.cost}</h6>
+                </div>
+                <div 
+                  style={{
+                    margin:"20px 0 ",
+                    display:"flex",
+                    alignItems:"center"
+                  }}
+                > 
+              <Image
+                src={logo}
+                alt="day trip"
+                height={15}
+                width={15}
+              ></Image>
+         <h6 className="text-lg font-semibold ml-1 ">{slide.trip}</h6>
+         </div>
+         <div>
+                  <button className="capitalize border-2  bg-[#43B97F] border-[#43B97F] text-white transition-all hover:ring-4 hover:ring-[#43B97F] font-medium rounded-3xl text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none">
+                    More about {slide.title}
+                  </button>
+         </div>
+
 
               </div>
          
--- src/components/template/home/subhome/BestPlaces.tsx
+++ src/components/template/home/subhome/BestPlaces.tsx
@@ -3,7 +3,7 @@ import SwiperComponent from '../../Carousal/Slider'
 
 function BestPlaces() {
   return (
-    <div className='w-full flex flex-col my-6 overflow-x-hidden'>
+    <div className='w-full flex flex-col my-9 overflow-x-hidden'>
         <h1  data-aos="fade-left" className="font-bold text-3xl md:text-6xl text-center my-8    background-image: url(/image/lake-como-italian-alke.jpg);  md:my-14 ">Top Destinations</h1>
       <SwiperComponent/>
     </div>
